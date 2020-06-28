3cfd2075f2d28b1328769f647178faa031d6ce7d
Alexandre Junior <jasj_jr@hotmail.com>
2020-06-27 21:50:38 -0300

    List transactions and balance

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -24,18 +24,53 @@ interface Transaction {
 }
 
 interface Balance {
-  income: string;
-  outcome: string;
-  total: string;
+  income: number;
+  outcome: number;
+  total: number;
+  formattedIncome: string;
+  formattedOutcome: string;
+  formattedTotal: string;
+}
+
+interface ApiResponse {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  // const [apiError, setApiError] = useState('');
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      try {
+        const response = await api.get<ApiResponse>('/transactions');
+
+        const apiTransactions = response.data.transactions.map(transaction => {
+          const { value, created_at } = transaction;
+
+          return {
+            ...transaction,
+            formattedValue: formatValue(value),
+            formattedDate: formatValue(created_at),
+          };
+        });
+
+        const apiBalance: Balance = {
+          ...response.data.balance,
+          formattedIncome: formatValue(response.data.balance.income),
+          formattedOutcome: formatValue(response.data.balance.outcome),
+          formattedTotal: formatValue(response.data.balance.total),
+        };
+
+        setTransactions(apiTransactions);
+        setBalance(apiBalance);
+        // setApiError('');
+      } catch (err) {
+        // setApiError('Error requesting transactions');
+        console.log('Error requesting transactions');
+      }
     }
 
     loadTransactions();
@@ -51,21 +86,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.formattedIncome}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.formattedOutcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.formattedTotal}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +116,18 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'income'
+                      ? transaction.formattedValue
+                      : `- ${transaction.formattedValue}`}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,14 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | Date): string => {
+  const localValue = Number(value);
+
+  if (Number.isNaN(localValue)) {
+    return Intl.DateTimeFormat('pt-BR').format(new Date(value));
+  }
+
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(localValue);
+};
 
 export default formatValue;
